2b85ea0562be968d36d5452cd5fc406c61a21a7a
Nokda <yh1483@naver.com>
2020-03-22 22:37:54 +0900

    ch8 modified

--- Book_1/ch8/express_query.js
+++ Book_1/ch8/express_query.js
@@ -5,7 +5,7 @@ var app = express()
 app.use(function(request, response, next){
     var name = request.query.name
     var region = request.query.region
-
+    // ex) localhost:52273?name=name&region=korea
     response.send('<h1>' + name + '-' + region + '</h1>')
 })
 
